3d7e5f13408a551ad082a391771dc5dd9263ca0b
Pedro Veloso <pedro95veloso@gmail.com>
2021-04-08 23:46:13 +0100

    das

--- form.js
+++ form.js
@@ -9,7 +9,7 @@ $("#SubmitButton").click(function(event) {
     }
 
     $.ajax({
-        url: 'https://teenhelper.herokuapp.com/api/0',
+        url: 'https://teenhelper.herokuapp.com/api/formResponse/0',
         type: 'Post',
         data: JSON.stringify(data),
         datatype: "json",
